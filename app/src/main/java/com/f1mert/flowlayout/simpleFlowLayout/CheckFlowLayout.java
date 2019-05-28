440f8fb9b6c6aa28a8f9f5304952dc2c4da7364f
734219084@qq.com <734219084@qq.com>
2019-05-28 23:01:43 +0800

    feat(init Project):create project
    -create flowlayout project

---
+++ app/src/main/java/com/f1mert/flowlayout/simpleFlowLayout/CheckFlowLayout.java
@@ -0,0 +1,28 @@
+package com.f1mert.flowlayout.simpleFlowLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * 多选流式布局
+ * * @author  f1mert
+ */
+public class CheckFlowLayout extends ViewGroup {
+    public CheckFlowLayout(Context context) {
+        super(context);
+    }
+
+    public CheckFlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
---
+++ app/src/main/java/com/f1mert/flowlayout/simpleFlowLayout/RadioFlowLayout.java
@@ -0,0 +1,20 @@
+package com.f1mert.flowlayout.simpleFlowLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioGroup;
+
+/**
+ * 单选流式布局
+ * @author  f1mert
+ */
+public class RadioFlowLayout extends RadioGroup {
+
+    public RadioFlowLayout(Context context) {
+        super(context);
+    }
+
+    public RadioFlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
